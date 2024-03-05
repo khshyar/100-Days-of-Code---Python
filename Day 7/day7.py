d21167b7975fc8180edf9ac5c29c5d562d622305
Khashayar Behnam <khshyarg@gmail.com>
2024-03-05 18:21:11 +0100

    day7

--- Day 7/day7.py
+++ Day 7/day7.py
@@ -1,23 +1,73 @@
 # step 1
 import random
+import os
+from hangman_words import word_list
+from hangman_art import stages, logo
 
 # TODO 1
-word_list = ["apple", "camel", "Lion"]
+# word_list = ["apple", "camel", "lion"]
 chosen_word = random.choice(word_list)
 list_of_letters = list(chosen_word)
+end_of_game = False
+display = []
+how_many_letters = len(list_of_letters)
+guess = ""
+lives = 6
+print(chosen_word)
+print(logo)
 
 # TODO 2
-guess = input("Guess a letter: ").lower()
+for n in range(0, how_many_letters):
+    display.append("_")
+print(display)
+
+while display != list_of_letters and not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    os.system("cls")
+
+    if guess in display:
+        print(f"You've already guessed this letter.\"{guess}\"")
+
+    for index, value in enumerate(list_of_letters):
+        if guess == value:
+            display[index] = guess
+
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+    print(display)
+    if "_" not in display:
+        print("You win.")
+    print(stages[lives])
 
 
-for n in list_of_letters:
-    if guess == n:
-        print("true")
-    else:
-        print("false")
+# another way to create loop
+# end_of_game = False
 
-print(type(guess))
-print(type(list_of_letters))
-print(chosen_word)
-print(list_of_letters)
+# while not end_of_game:
+#     guess = input("Guess a letter: ").lower()
+#     for index, value in enumerate(list_of_letters):
+#         if guess == value:
+#             display[index] = guess
+#     print(display)
+
+#     if "_" not in display:
+#         end_of_game = True
+
+# Another way to create a loop with index
+# sequence = ['apple', 'banana', 'cherry']
+# for index in range(len(sequence)):
+#     value = sequence[index]
+#     print(f"Index: {index}, Value: '{value}'")
+
+
+# print(display)
+# print(type(guess))
+# print(type(list_of_letters))
+
+# print(list_of_letters)
 # print(guess)
