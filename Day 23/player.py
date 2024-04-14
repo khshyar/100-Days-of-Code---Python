ea4c3e5806945841fdf3301f904757ab56f822c5
Khashayar Behnam <khshyarg@gmail.com>
2024-04-14 13:57:39 +0200

    day23

--- Day 23/player.py
+++ Day 23/player.py
@@ -1,7 +1,7 @@
 from turtle import Turtle
 
 STARTING_POSITION = (0, -280)
-MOVE_DISTANCE = 10
+MOVE_DISTANCE = 25
 FINISH_LINE_Y = 280
 
 
@@ -10,14 +10,18 @@ class Player(Turtle):
         super().__init__()
         self.shape("turtle")
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.start_point()
         self.setheading(90)
 
     def move(self):
-        self.fd(10)
-        if self.ycor() == 280:
-            self.goto(STARTING_POSITION)
+        self.fd(MOVE_DISTANCE)
+
+    def start_point(self):
+        self.goto(STARTING_POSITION)
 
     def next_level(self):
-        if self.ycor() == 280:
-            self.goto(STARTING_POSITION)
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
+            # self.goto(STARTING_POSITION)
